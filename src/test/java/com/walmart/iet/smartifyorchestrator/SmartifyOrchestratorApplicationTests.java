78d48b6802a65dd195a6a38f3324357fbc4da157
dhankar <dhankar@walmart.com>
2022-06-15 16:24:39 -0700

    First commit for cerating the project with H2 intialization

---
+++ src/test/java/com/walmart/iet/smartifyorchestrator/SmartifyOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.iet.smartifyorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartifyOrchestratorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
